f1677215ca261c98d8a9442f263863e37283c590
Franson33 <antonchikfr@gmail.com>
2021-10-28 13:39:54 +0300

    Add spring animation to Cube hover and scale.

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { useState, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useSpring, a } from "@react-spring/three";
 
 function Cube(props: JSX.IntrinsicElements["mesh"]): JSX.Element {
   const ref = useRef<THREE.Mesh>(null!);
@@ -12,18 +13,23 @@ function Cube(props: JSX.IntrinsicElements["mesh"]): JSX.Element {
     ref.current.rotation.y += 0.01;
   });
 
+  const spring = useSpring({
+    scale: active ? 2 : 1,
+    color: hover ? "#ee8a8a" : "#a9f2af",
+  });
+
   return (
-    <mesh
+    <a.mesh
       {...props}
       ref={ref}
-      scale={active ? 2 : 1}
+      scale={spring.scale}
       onClick={(evt) => setActive(!active)}
       onPointerOver={(evt) => setHover(true)}
       onPointerOut={(evt) => setHover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hover ? "#ee8a8a" : "#a9f2af"} />
-    </mesh>
+      <a.meshStandardMaterial color={spring.color} />
+    </a.mesh>
   );
 }
 

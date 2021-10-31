15ac1fe4c1b6640efe971cec708920c3f7e2b0d2
Franson33 <antonchikfr@gmail.com>
2021-10-31 20:29:28 +0200

    Move onCreate function inside canvas from render.

--- src/screens/ControlledCube.tsx
+++ src/screens/ControlledCube.tsx
@@ -4,15 +4,18 @@ import CubeControlled from "../components/CubeControlled";
 import Controls from "../components/Controls";
 import Plane from "../components/Plane";
 
+interface shadowMapPropsInterface {
+  gl: THREE.WebGLRenderer;
+}
+
 function ControlledCube(): JSX.Element {
+  const shadowMap = ({ gl }: shadowMapPropsInterface) => {
+    gl.shadowMap.enabled = true;
+    gl.shadowMap.type = THREE.PCFSoftShadowMap;
+  };
+
   return (
-    <StyledCanvas
-      backColor={"#f0f0f0"}
-      onCreated={({ gl }) => {
-        gl.shadowMap.enabled = true;
-        gl.shadowMap.type = THREE.PCFSoftShadowMap;
-      }}
-    >
+    <StyledCanvas backColor={"#f0f0f0"} onCreated={shadowMap}>
       <ambientLight />
       <spotLight intensity={5} position={[5, 5, 5]} penumbra={1} castShadow />
       <Controls />
